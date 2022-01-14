ded028763648ce72363722dd8952ea5ac93f97e1
tarwat-uddin <t.uddin9121@gmail.com>
2022-01-14 20:27:30 +0000

    Add new API endpoint

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,1 +1,2 @@
-export const API_URL = `https://en.wikipedia.org/w/api.php`;
+export const API_URL_WIKI = `https://en.wikipedia.org/w/api.php`;
+export const API_URL_QUOTE = `https://type.fit/api/quotes`;
