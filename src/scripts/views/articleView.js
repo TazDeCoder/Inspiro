29a71ec0a7d63b3bc33b01870ab9aecab02093e7
tarwat-uddin <t.uddin9121@gmail.com>
2022-01-15 12:47:55 +0000

    Refractor articleView.js and searchView.js
    Major improvements

--- src/scripts/views/articleView.js
+++ src/scripts/views/articleView.js
@@ -1,33 +1,45 @@
 "use strict";
 
 import templates from "../templates.js";
-import { contentMain } from "../controller.js";
 
-function articleView() {
+export default function buildArticleView() {
+  let listEl;
+  // Creating base parent
+  const base = document.createElement("div");
   // Private methods
-  const generateArticleMarkup = ({ title, image, contentsArr, contextArr }) => {
-    const articleInput = {
-      title,
-      image,
-      contentsArr,
-      contextArr,
-    };
-    const template = Handlebars.compile(templates.article2());
+  const generateArticleMarkup = ({
+    title,
+    imageSrc,
+    pageContents,
+    pageText,
+  }) => {
+    const articleInput = { title, imageSrc, pageContents, pageText };
+    const template = Handlebars.compile(templates.article());
     return template(articleInput);
   };
-  const clearContent = () => (contentMain.innerHTML = "");
+
+  const clear = () => (base.innerHTML = "");
+  // Add event handler
+  const handleClick = function (e) {
+    e.preventDefault();
+    const clicked = e.target.closest(".list__item-link");
+    if (!clicked) return;
+    const hash = clicked.hash.slice(1);
+    document.getElementById(hash).scrollIntoView({ behavior: "smooth" });
+  };
   // Public methods
   const render = function (data) {
-    if (!data || (Array.isArray(data) && data.length === 0));
+    if (!data || data.length === 0);
     const articleMarkup = generateArticleMarkup(data);
-    clearContent();
-    contentMain.insertAdjacentHTML("afterbegin", articleMarkup);
+    clear();
+    base.innerHTML = articleMarkup;
+    listEl = base.querySelector(".head__list");
+    listEl.addEventListener("click", handleClick);
   };
   // Public API
   const publicApi = {
     render,
+    base,
   };
   return publicApi;
 }
-
-export default articleView();
--- src/scripts/views/searchView.js
+++ src/scripts/views/searchView.js
@@ -2,22 +2,35 @@
 
 function searchView() {
   // Private variables
+  const headEl = document.querySelector(".head--top");
   const parentEl = document.querySelector(".content__form--search");
   const iconEl = document.querySelector(".form__icon--search");
   const inputEl = document.querySelector(".form__input--field");
+  const headHeight = headEl.getBoundingClientRect().height;
   // Private methods
+  // Add event listener
+  window.addEventListener("scroll", function () {
+    const currentScrollPos = window.pageYOffset;
+    if (currentScrollPos > headHeight + 100) {
+      headEl.classList.add("head--hide");
+    } else {
+      headEl.classList.remove("head--hide");
+    }
+  });
   // Public methods
   const addHandlerSearch = function (handler) {
     parentEl.addEventListener("submit", function (e) {
       e.preventDefault();
       const query = inputEl.value;
       if (query === "") return inputEl.focus();
+      inputEl.value = "";
       handler(query);
     });
     iconEl.addEventListener("click", function (e) {
       e.preventDefault();
       const query = inputEl.value;
       if (query === "") return inputEl.focus();
+      inputEl.value = "";
       handler(query);
     });
   };
